f4ce31b61612041c42876cd2be6cc95ca1b6aa02
Fariba DK <faribacodes@gmail.com>
2021-04-24 17:02:40 -0700

    Sat commits

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 //~~~~~~~~ CREATE A PET CLASS ~~~~~~~~
-
+console.log('hi')
 class Pet {
   constructor(character){
     this.character = character;
@@ -12,35 +12,58 @@ class Pet {
 const pet = new Pet('Pixie');
 console.log(pet)
 
+document.getElementById('start').addEventListener('click', function(event){
+  document.querySelector(".controls").classList.remove('hidden')
+  event.target.remove();
+})
+
+
+function counter(n){
+  let timer = 0;
+
+  setInterval(function(){
+    if(timer == 11){
+      clearInterval(this)
+      console.log('Game Over!')
+    }else{
+      console.log('Time is kicking ', timer++)
+    }
+  }, 1000)
+}
+let n = 10
+console.log(counter(n))
+
+$('#start').on('click', function(){
+  counter()
+  console.log('tick tock')
+})
 
 //~~~~~~~~~  START - INSTANTIATE PET - SET TIMER ~~~~~~~~~~~~
-$(`.start`).on('click', function () {
-  console.log('this is working');
-});
+
 //check to see if .start button works
 
-let timer = 0;
 //input for pet
 //submit button --> name
 //wire up the function to DIE -> black background with a white text - your pet died
 //annimation
 
+
+
 function startFunction(){
 
   const counter = setInterval(()=>{
-    timer++;
-    age++;
-    pet.hungry++;
-      $('#hunger').val(pet.hungry)
 
-    pet.boredom++;
-      $('#boredom').val(pet.boredom)
+    // age++;
+    // pet.hungry++;
+    //   $('#hunger').val(pet.hungry)
 
-    pet.sleepy++;
-      $('#sleepy').val(pet.sleepy)
+    // pet.boredom++;
+    //   $('#boredom').val(pet.boredom)
 
-    console.log(timer)
-    $('.timer').text(timer)
+    // pet.sleepy++;
+    //   $('#sleepy').val(pet.sleepy)
+
+
 
     if (pet.hungry > 11 || pet.boredom > 11 || pet.sleep > 11){
       //set interval runs every sec
@@ -58,18 +81,18 @@ $('.increment-btn-hunger').click(function () {
   $('#hunger').val(pet.hungry)
 })}
 
-function rest(){
-$('.increment-btn-sleep').click(function () {
-  pet.sleep--;
-  $('#sleep').val(pet.sleep)
-})}
+// function rest(){
+// $('.increment-btn-sleep').click(function () {
+//   pet.sleep--;
+//   $('#sleep').val(pet.sleep)
+// })}
 
-function play(){
-$('.increment-btn-boredom').click(function () {
-  pet.boredom--;
-  $('#boredom').val(pet.boredom)
-})
-}
+// function play(){
+// $('.increment-btn-boredom').click(function () {
+//   pet.boredom--;
+//   $('#boredom').val(pet.boredom)
+// })
+// }
 
 
 /*
