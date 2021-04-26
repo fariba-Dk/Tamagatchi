ecec8eab8b22b76fd719498169ad6ee5fbb78d67
Fariba DK <faribacodes@gmail.com>
2021-04-26 15:13:45 -0700

    april 26th

--- README.md
+++ README.md
@@ -32,6 +32,8 @@ MDN Javascript Docs (a great reference for all things Javascript)
 jQuery Docs (if you're using jQuery)
 Github Pages (for hosting your game)
 Example Readme Files (a great reference for planning materials)
+
+
 Suggested Ways to Get Started
 Break the project down into different components (data, presentation, views, style, DOM manipulation) and brainstorm each component individually. Use whiteboards!
 Use your Development Tools (console.log, element inspector, alert statements, etc) to debug and solve problems
@@ -42,10 +44,13 @@ Plan to write code that you know you will have to remove later. Create temporary
 Specifications
 Create a repo for your tamagotchi pet
 make a commit after you finish each one of the following
+
+
 Create a Class (JS Class, look at your notes if your forget) for your tamagotchi
 Instatiate your Tamagotchi
 Display a character of your choice on the screen to represent your pet
 Display the following metrics for your pet:
+
 Hunger (1-10 scale)
 Sleepiness (1-10 scale)
 Boredom (1-10 scale)
@@ -57,3 +62,10 @@ Increase your pet's age every x minutes
 Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
 You pet should die if Hunger, Boredom, or Sleepiness hits 10.
 Animate your pet across the screen while it's alive.
+
+Extras
+Morph your pet at certain ages.
+Have your tamagotchi give birth to baby tamagotchi...
+...with special powers (extend the class)!
+Add an excercise() method to your tamagotchi, that affects certain properties
+Add anything you can think of... use your imagination!
--- scripts/app.js
+++ scripts/app.js
@@ -1,129 +1,106 @@
-//~~~~~~~~ CREATE A PET CLASS ~~~~~~~~
-console.log('hi')
 class Pet {
-  constructor(character){
+  constructor(character) {
     this.character = character;
-    this.age = 0;// start -> 0/1
-    this.boredom = 0;
-    this.hungry = 0;
-    this.sleep = 0;
+    this.age = 0; // start -> 0/1
+    this.boredom = 20;
+    this.hungry = 20;
+    this.sleep = 20;
   }
 }
 const pet = new Pet('Pixie');
-console.log(pet)
+console.log(pet);
 
-document.getElementById('start').addEventListener('click', function(event){
-  document.querySelector(".controls").classList.remove('hidden')
+//the egg hatch and the pet appears
+document.getElementById('start').addEventListener('click', function (event) {
+  document.querySelector('.controls').classList.remove('hidden');
   event.target.remove();
-})
+});
 
 
-function counter(n){
+function counter() {
   let timer = 0;
 
-  setInterval(function(){
-    if(timer == 11){
-      clearInterval(this)
-      console.log('Game Over!')
-    }else{
-      console.log('Time is kicking ', timer++)
+  let timerIntervals = setInterval(function () {
+     timer++;
+     pet.age++;
+    if (timer == 30) {
+      clearInterval(timerIntervals);
+      console.log('Game Over!');
+    } else {
+      // console.log('Time is kicking ' + timer);
+      $('#timer').text(timer);
+      $('#age').text(pet.age)
     }
-  }, 1000)
+  }, 1000);
 }
-let n = 10
-console.log(counter(n))
 
-$('#start').on('click', function(){
-  counter()
-  console.log('tick tock')
-})
+$('#start').on('click', function () {
+  counter();
+});
 
-//~~~~~~~~~  START - INSTANTIATE PET - SET TIMER ~~~~~~~~~~~~
+//pet.age++;~~~~~~~~~~~~~~~~~~~~~~~~~~~~ another place
+function hungerFunction() {
+  const counter = setInterval(() => {
 
-//check to see if .start button works
-
-//input for pet
-//submit button --> name
-//wire up the function to DIE -> black background with a white text - your pet died
-//annimation
-
-
-
-function startFunction(){
-
-  const counter = setInterval(()=>{
-
-    // age++;
-    // pet.hungry++;
-    //   $('#hunger').val(pet.hungry)
-
-    // pet.boredom++;
-    //   $('#boredom').val(pet.boredom)
-
-    // pet.sleepy++;
-    //   $('#sleepy').val(pet.sleepy)
-
-
-
-    if (pet.hungry > 11 || pet.boredom > 11 || pet.sleep > 11){
+    pet.hungry++;
+   // $('#hunger').text(pet.hungry);
+   $("#hunger-bar").val(pet.hungry)
+    if (pet.hungry > 51) {
       //set interval runs every sec
       alert(`Game Over!`);
       clearInterval(counter);
     }
   }, 3000);
 }
-startFunction()
+hungerFunction();
 
-
-function feedMe(){
-$('.increment-btn-hunger').click(function () {
+function feedMe() {
+  console.log('Hi im feedMe');
   pet.hungry--;
-  $('#hunger').val(pet.hungry)
-})}
+  $('#hunger-bar').val(pet.hungry);
 
-// function rest(){
-// $('.increment-btn-sleep').click(function () {
-//   pet.sleep--;
-//   $('#sleep').val(pet.sleep)
-// })}
-
-// function play(){
-// $('.increment-btn-boredom').click(function () {
-//   pet.boredom--;
-//   $('#boredom').val(pet.boredom)
-// })
-// }
+}
+$('#food').on("click", feedMe);
 
 
-/*
-img src="https://media1.tenor.com/images/8ab88b79885ab587f84cbdfbc3b87835/tenor.gif?itemid=15917800""
-        <!--
-*/
+function boredFunction() {
+  const counter = setInterval(() => {
+    pet.boredom++;
+    $('#bored').text(pet.boredom);
 
+    if (pet.boredom > 31) {
+      //set interval runs every sec
+      window(`Game Over!`);
+      clearInterval(counter);
+    }
+  }, 3000);
+}
+boredFunction();
 
+function playWithMe() {
+  pet.boredom--;
+  $('#bored').text(pet.boredom);
+}
+$('.play').on(playWithMe);
 
+function sleepFunction() {
+  const counter = setInterval(() => {
+    pet.tired++;
+    $('#tired').text(pet.sleep);
+    if (pet.sleep > 31) {
+      //set interval runs every sec
+      alert(`Game Over!`);
+      clearInterval(counter);
+    }
+  }, 3000);
+}
+sleepFunction();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function rest(){
+  pet.sleep--;
+  $('#tired').text(pet.sleep)
+}
+$('.sleep').click(rest())
 
 
 
@@ -133,5 +110,15 @@ img src="https://media1.tenor.com/images/8ab88b79885ab587f84cbdfbc3b87835/tenor.
 
 
 
-
-
+//~~~~~~~~ CREATE A PET CLASS ~~~~~~~~
+// Hunger (1-10 scale)
+// Sleepiness (1-10 scale)
+// Boredom (1-10 scale)
+// Age
+// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
+// Add the ability to name your pet.
+// Style the page.
+// Increase your pet's age every x minutes
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+// Animate your pet across the screen while it's alive.
